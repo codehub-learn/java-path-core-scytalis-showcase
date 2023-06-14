e0c9fdb6a57908a7bc2ac555277545a8efcdabea
Ioannis <jondan97@gmail.com>
2023-06-14 17:14:33 +0300

    added exception showcase

---
+++ java-path-core-exception/src/main/java/gr/codelearn/BusinessException.java
@@ -0,0 +1,7 @@
+package gr.codelearn;
+
+public class BusinessException extends Exception {
+	public BusinessException(final String message) {
+		super(message);
+	}
+}
--- java-path-core-exception/src/main/java/gr/codelearn/Main.java
+++ java-path-core-exception/src/main/java/gr/codelearn/Main.java
@@ -3,13 +3,67 @@ package gr.codelearn;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Main {
 
 	private static final Logger logger = LoggerFactory.getLogger(Main.class);
 	public static void main(String[] args) {
-		logger.info("hello");
-		final ArrayList<Object> objects = new ArrayList<>();
+		try {
+			final String[] strings = new String[10];
+			final String string = strings[13];
+			Integer.parseInt("5a");
+			// ...
+			// ...
+			// ...
+			logger.info("hello");
+			logger.info("hello");
+		}
+		catch(ArrayIndexOutOfBoundsException e){
+			logger.error("Array index out of bounds");
+		}
+
+
+		logger.info("outside try");
+		try {
+			serviceMethod();
+		} catch (BusinessException e) {
+			// redirect page 404 (for example)
+			logger.error(e.getMessage());
+		}
+	}
+
+	private static void readFile(){
+		try {
+			final FileReader fileReader = new FileReader("/C://...");
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static void connectToDatabase(){
+		try {
+			final Connection connection = DriverManager.getConnection("...");
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static void repositoryMethod() throws SQLException {
+		DriverManager.getConnection("...");
+	}
+
+	private static void serviceMethod() throws BusinessException {
+		try {
+			repositoryMethod();
+		} catch (SQLException e) {
+			logger.error(e.getMessage());
+			throw new BusinessException("Cannot connect to database");
+		}
 	}
 }
